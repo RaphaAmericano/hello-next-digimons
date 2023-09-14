042b759af8256534b6ea619d030a8491a0c0be10
Raphael Americano <raphaelamericano@Raphaels-MacBook-Pro.local>
2023-09-13 22:24:35 -0300

    remove configs

--- next.config.js
+++ next.config.js
@@ -1,27 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'digimon.shadowsmith.com',
-                port: '',
-                pathname:'/img/**'
-            }
-        ]
-    },
-    publicRuntimeConfig: {
-        apiUrl: process.env.API_URL
-    },
-    experimental:{
-        images: {
-            allowFutureImages: true
-        },
-        externalDir: true | {
-            enable: true,
-            silent: true
-        }
-    }
+    // images: {
+    //     remotePatterns: [
+    //         {
+    //             protocol: 'https',
+    //             hostname: 'digimon.shadowsmith.com',
+    //             port: '',
+    //             pathname:'/img/**'
+    //         }
+    //     ]
+    // },
+    // publicRuntimeConfig: {
+    //     apiUrl: process.env.API_URL
+    // },
+    // experimental:{
+    //     images: {
+    //         allowFutureImages: true
+    //     },
+    //     externalDir: true | {
+    //         enable: true,
+    //         silent: true
+    //     }
+    // }
 }
 
 module.exports = nextConfig
